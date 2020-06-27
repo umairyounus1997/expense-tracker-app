dfdf2d130f3a017583e09d3d7e1f194859c2ebbb
Umair Younus <41501844+umairyounus1997@users.noreply.github.com>
2020-06-27 22:29:18 +0500

    Changes

--- src/App.js
+++ src/App.js
@@ -2,10 +2,13 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+//Import Components
+import {Header} from './components/Header'
+
 function App() {
   return (
     <div>
-      Hello World
+      <Header></Header>
     </div>
   );
 }
---
+++ src/components/Header.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export const Header = () => {
+    return (
+        <h1>
+            Expense Tracker App Presented by Umair Younus
+        </h1>
+    )
+}
+export default Header;
